dc89a42a22f357891d5364dbf4482000614df732
Zepeacedust <arnor0903@gmail.com>
2022-11-05 18:06:35 +0000

    villur í boilerplate vengna þess að það var úr öðru verkefni

--- ApiService/main.py
+++ ApiService/main.py
@@ -12,4 +12,4 @@ def create_app() -> FastAPI:
 app = create_app()
 
 if __name__ == '__main__':
-    uvicorn.run('application:app', host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
