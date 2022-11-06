d43643462c8dd39aa9d3fcd5db3e69439040dafb
Zepeacedust <arnor0903@gmail.com>
2022-11-06 21:14:55 +0000

    help

--- InventoryService/main.py
+++ InventoryService/main.py
@@ -21,7 +21,9 @@ def create_app() -> FastAPI:
 app = create_app()
 
 if __name__ == '__main__':
+    print("Starting event watcher")
     event_watch = EventWatcher()
-    event_thread = threading.Thread(target=EventWatcher.start, args=[event_watch])
+    event_thread = threading.Thread(target=EventWatcher.start, kwargs={"self":event_watch})
     event_thread.start()
+    print("Event watcher successfully started")
     uvicorn.run('main:app', host='0.0.0.0', port=8000)
--- PaymentService/main.py
+++ PaymentService/main.py
@@ -1,1 +1,16 @@
+import pika
+from time import sleep
+from retry import retry
 
+@retry(pika.exceptions.AMQPConnectionError, delay=5, jitter=(1, 3))
+def get_connection():    
+    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+    channel=connection.channel()
+    channel.exchange_declare(exchange='payment', exchange_type='direct')
+    return channel
+
+channel = get_connection()
+while True:
+    channel.basic_publish(exchange="payment", routing_key="payment.success", body="yeee")
+    sleep(3)
+    
