0562bf709ab36a115d3223fcf91942de5eb68a58
Zepeacedust <arnor0903@gmail.com>
2022-11-06 20:15:29 +0000

    attempt to fix inventory service

--- InventoryService/main.py
+++ InventoryService/main.py
@@ -21,6 +21,7 @@ def create_app() -> FastAPI:
 app = create_app()
 
 if __name__ == '__main__':
-    event_thread = threading.Thread(target=EventWatcher)
+    event_watch = EventWatcher()
+    event_thread = threading.Thread(target=EventWatcher.start, args=[event_watch])
     event_thread.start()
     uvicorn.run('main:app', host='0.0.0.0', port=8000)
